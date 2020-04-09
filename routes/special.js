d2ca4a260d6aa0449f7722c1dbca8eda1381caac
zhoujin <171226Q2>
2020-04-09 11:38:03 +0800

    专题页面基本结构

---
+++ routes/special.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var router = express.Router();
+var specialController = require('../controllers/special');
+var error = require('../common/error');
+
+//专题展示页面.
+router.get('/node/s/123.html',function(req , res , next){
+    console.log('专题页')
+    try{
+        console.log("页面请求开始.......");
+        console.time();
+        specialController.render(req , res);
+        console.timeEnd();
+    }catch(e){
+        error(req , res , next);
+    }
+});
+module.exports = router;
