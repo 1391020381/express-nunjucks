665035f25f8383c5c1205f74133a96416c813c8b
zhoujin <171226Q2>
2020-04-10 16:02:49 +0800

    合并代码

--- public/javascripts/src/special/bottomBar.js
+++ public/javascripts/src/special/bottomBar.js
@@ -1,3 +1,5 @@
 define(function(require , exports , module){
     console.log('专题的bottomBar')
+    var method = require("../application/method");
+   console.log('method:', method,method.getCookie('cuk'))
 });
