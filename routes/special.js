0a198211f3332bcd5f943f50512f3f449122755b
zhoujin <171226Q2>
2020-04-10 11:13:10 +0800

    专题页面,bottomBar的基本结构

--- routes/special.js
+++ routes/special.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 var specialController = require('../controllers/special');
 var error = require('../common/error');
-
 //专题展示页面.
 router.get('/node/s/123.html',function(req , res , next){
     console.log('专题页')
