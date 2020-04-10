ff1ff692d8d8a0103043e823950ee3de8085ab20
xiaowen <wen.xiao@iask.cn>
2020-04-10 15:47:30 +0800

    专题页面结构编写

--- controllers/special.js
+++ controllers/special.js
@@ -6,10 +6,58 @@ var urlencode = require('urlencode');
 //测试
 module.exports = {
     render: function(res, req) {
-            console.log('1---------1')
-            return render("special/index", {},res, req);
-            // return async.series({}, function (err, results) {
-            //     render("special/index", results, res, req);
-            // })
+ 
+            return async.series({
+                list:function(callback){
+                    callback()
+                }
+            }, function (err, results) {
+                var results = results || {};
+                results.condition = [
+                    {
+                        title: '格式',
+                        code: 'format',
+                        content: [
+                            { code: '', des: '全部', active: true },
+                            { code: 'doc', des: 'Word', active: false },
+                            { code: 'xls', des: 'Excel', active: false },
+                            { code: 'ppt', des: 'PPT', active: false },
+                            { code: 'txt', des: 'TXT', active: false },
+                            { code: 'pdf', des: 'PDF', active: false }
+                        ]
+                    },
+                    {
+                        title: '范围',
+                        code: 'fileType',
+                        content: [
+                            { code: '', des: '全部', active: true },
+                            { code: 'highQuality', des: '精选', active: false },
+                            { code: 'downloadVoucher', des: '下载券', active: false },
+                            { code: 'vipExclusive', des: 'VIP专享', active:false },
+                            { code: 'pay', des: '付费', active: false }
+                        ]
+                    },
+                    {
+                        title: '分类',
+                        code: 'fileType',
+                        content: [
+                            { code: '', des: '全部', active: true },
+                            { code: 'highQuality', des: '精选', active: false },
+                            { code: 'downloadVoucher', des: '下载券', active: false },
+                            { code: 'vipExclusive', des: 'VIP专享', active:false },
+                            { code: 'pay', des: '付费', active: false }
+                        ]
+                    }
+                ];
+                //最大20页
+                var pageIndexArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+                // if (results.list.data.totalPages < 20) {
+                //     pageIndexArr.length = results.list.data.totalPages;
+                // }
+                results.pageIndexArr=pageIndexArr;
+
+                render("special/index", results, res, req);
+            })
     }
 }
--- routes/special.js
+++ routes/special.js
@@ -4,13 +4,10 @@ var specialController = require('../controllers/special');
 var error = require('../common/error');
 
 //专题展示页面.
-router.get('/node/s/123.html',function(req , res , next){
-    console.log('专题页')
+router.get('/node/s/*.html',function(req , res , next){
     try{
-        console.log("页面请求开始.......");
-        console.time();
-        specialController.render(req , res);
-        console.timeEnd();
+        specialController.render(req, res);
+    
     }catch(e){
         error(req , res , next);
     }
