0ae122c7d7c27c2d1088df185fa68b1110250110
chenkun <kun.chen@iask.cn>
2020-05-22 15:35:32 +0800

    updated

--- public/javascripts/src/upload/upload.js
+++ public/javascripts/src/upload/upload.js
@@ -43,24 +43,31 @@ define(function(require , exports , module){
                 $('.permin').hide();
                 $('.money').hide();
             })
+             // 登录
+            $('.user-login,.login-open-vip').on('click', function () {
+                if (!utils.getCookie('cuk')) {
+                    login.notifyLoginInterface(function (data) {
+                        uploadObj.refreshTopBar(data);
+                    });
+                }
+            });
         },
         beforeInit:function(){
             if (!utils.getCookie('cuk')) {
                 login.notifyLoginInterface(function (data) {
-                    alert(1111111)
+                   console.log(data)
                    if (data) {
                         uploadObj.isAuth = data.isAuth =="0" ? false: true;
-                        refreshTopBar(data);
+                        uploadObj.refreshTopBar(data);
                    }
                 });
             }else {
-                login.syncUserInfoInterface(function (data) {
-                    lert(22222)
+                login.getLoginData(function (data) {
                     if (data) {
-                         uploadObj.isAuth = data.isAuth =="0" ? false: true;
-                         refreshTopBar(data);
-                    }
-                 });
+                        uploadObj.isAuth = data.isAuth =="0" ? false: true;
+                        uploadObj.refreshTopBar(data);
+                   }
+                });
             }
         },
         checkHook:function(){
@@ -714,78 +721,69 @@ define(function(require , exports , module){
                }
             })
            
+        },
+         //刷新topbar
+        refreshTopBar:function (data) {
+            var $unLogin = $('#unLogin');
+            var $hasLogin = $('#haveLogin');
+            var $btn_user_more = $('.btn-user-more');
+            var $vip_status = $('.vip-status');
+            var $icon_iShare = $(".icon-iShare");
+            var $top_user_more = $(".top-user-more");
+
+            $btn_user_more.text(data.isVip == 1 ? '续费' : '开通');
+            var $target = null;
+
+            //VIP专享资料
+            if (method.getCookie('file_state') === '6') {
+                $('.vip-title').eq(0).show();
+            }
+
+            //vip
+            if (data.isVip == 1) {
+                $target = $vip_status.find('p[data-type="2"]');
+                $target.find('.expire_time').html(data.expireTime);
+                $target.show().siblings().hide();
+                $top_user_more.addClass('top-vip-more');
+                $('.isVip-show').find('span').html(data.expireTime);
+                $('.isVip-show').removeClass('hide');
+                //vip 已经 过期
+            } else if (data.userType == 1) {
+                $target = $vip_status.find('p[data-type="3"]');
+                $hasLogin.removeClass("user-con-vip");
+                $target.show().siblings().hide();
+                // 新用户
+            } else if (data.isVip == 0) {
+                $hasLogin.removeClass("user-con-vip");
+
+                // 用户不是vip,但是登录啦,隐藏 登录后开通 显示 开通
+                $('.btn-join-vip').eq(0).hide()
+                $('.btn-join-vip').eq(1).show()
+                // 续费vip
+            } else if (data.isVip == 2) {
+                $('.vip-title').hide();
+            }
+
+            $unLogin.hide();
+            $hasLogin.find('.user-link .user-name').html(data.nickName);
+            $hasLogin.find('.user-link img').attr('src', data.weiboImage);
+            $hasLogin.find('.top-user-more .name').html(data.nickName);
+            $hasLogin.find('.top-user-more img').attr('src', data.weiboImage);
+            $hasLogin.show();
+
+            window.pageConfig.params.isVip = data.isVip;
+            var fileDiscount = data.fileDiscount;
+            if (fileDiscount) {
+                fileDiscount = fileDiscount / 100;
+            } else {
+                fileDiscount = 0.8;
+            }
+            window.pageConfig.params.fileDiscount = fileDiscount;
+            $("#ip-uid").val(data.userId);
+            $("#ip-isVip").val(data.isVip);
+            $("#ip-mobile").val(data.mobile);
         }
     }
    
-
-     // 登录
-     $('.user-login,.login-open-vip').on('click', function () {
-        if (!utils.getCookie('cuk')) {
-            login.notifyLoginInterface(function (data) {
-                refreshTopBar(data);
-            });
-        }
-    });
-    //刷新topbar
-var refreshTopBar = function (data) {
-    var $unLogin = $('#unLogin');
-    var $hasLogin = $('#haveLogin');
-    var $btn_user_more = $('.btn-user-more');
-    var $vip_status = $('.vip-status');
-    var $icon_iShare = $(".icon-iShare");
-    var $top_user_more = $(".top-user-more");
-
-    $btn_user_more.text(data.isVip == 1 ? '续费' : '开通');
-    var $target = null;
-
-    //VIP专享资料
-    if (method.getCookie('file_state') === '6') {
-        $('.vip-title').eq(0).show();
-    }
-
-    //vip
-    if (data.isVip == 1) {
-        $target = $vip_status.find('p[data-type="2"]');
-        $target.find('.expire_time').html(data.expireTime);
-        $target.show().siblings().hide();
-        $top_user_more.addClass('top-vip-more');
-        $('.isVip-show').find('span').html(data.expireTime);
-        $('.isVip-show').removeClass('hide');
-        //vip 已经 过期
-    } else if (data.userType == 1) {
-        $target = $vip_status.find('p[data-type="3"]');
-        $hasLogin.removeClass("user-con-vip");
-        $target.show().siblings().hide();
-        // 新用户
-    } else if (data.isVip == 0) {
-        $hasLogin.removeClass("user-con-vip");
-
-        // 用户不是vip,但是登录啦,隐藏 登录后开通 显示 开通
-        $('.btn-join-vip').eq(0).hide()
-        $('.btn-join-vip').eq(1).show()
-        // 续费vip
-    } else if (data.isVip == 2) {
-        $('.vip-title').hide();
-    }
-
-    $unLogin.hide();
-    $hasLogin.find('.user-link .user-name').html(data.nickName);
-    $hasLogin.find('.user-link img').attr('src', data.weiboImage);
-    $hasLogin.find('.top-user-more .name').html(data.nickName);
-    $hasLogin.find('.top-user-more img').attr('src', data.weiboImage);
-    $hasLogin.show();
-
-    window.pageConfig.params.isVip = data.isVip;
-    var fileDiscount = data.fileDiscount;
-    if (fileDiscount) {
-        fileDiscount = fileDiscount / 100;
-    } else {
-        fileDiscount = 0.8;
-    }
-    window.pageConfig.params.fileDiscount = fileDiscount;
-    $("#ip-uid").val(data.userId);
-    $("#ip-isVip").val(data.isVip);
-    $("#ip-mobile").val(data.mobile);
-};
 uploadObj.init();
 });
