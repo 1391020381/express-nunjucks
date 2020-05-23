2773dc679ec223f85d3e730b4ac1c76d8c171800
chenkun <kun.chen@iask.cn>
2020-05-23 16:15:36 +0800

    updated

--- public/javascripts/src/upload/upload.js
+++ public/javascripts/src/upload/upload.js
@@ -51,6 +51,11 @@ define(function(require , exports , module){
                     });
                 }
             });
+            // 退出登录
+            $('.btn-exit').click(function(){
+                login.ishareLogout()
+            })
+           
             // 头部搜索跳转
         },
         beforeInit:function(){
@@ -429,27 +434,29 @@ define(function(require , exports , module){
                 var priceVal = $(this).val();
                 if(!priceVal){
                     $(this).siblings('.select-item-info').show().text('请输入金额')
-                    return false;
                 }else if (priceVal<1) {
                     $(this).siblings('.select-item-info').show().text('金额必须大于0')
-                    return false;
                 }
                 var itemIndex = $(event.target).parents('.doc-li').attr('index');
                 if (itemIndex>-1) {
-                    uploadObj.uploadFiles[itemIndex].userFilePrice = priceVal;
+                    if(priceVal>0) {
+                        uploadObj.uploadFiles[itemIndex].userFilePrice = priceVal;
+                    }
                 }else {
-                    uploadObj.uploadFiles.forEach(function(item){
-                        if(item.checked) {
-                            item.userFilePrice =  priceVal;
-                        }
-                    })
+                    if(priceVal>0) {
+                        uploadObj.uploadFiles.forEach(function(item){
+                            if(item.checked) {
+                                item.userFilePrice =  priceVal;
+                            }
+                        })
+                    }
                 }
             })
             $('.js-file-item').on('blur',".doc-pay-input input[name='moneyPrice']",function(){
                 uploadObj.publicFileRener() 
             })
            
-            $('.doc-batch-fixed').on('keyup',".doc-pay-input input[name='moneyPrice']",function(){
+            $('.doc-batch-fixed').on('blur',".doc-pay-input input[name='moneyPrice']",function(){
                 var priceVal = $(this).val();
                 uploadObj.uploadFiles.forEach(function(item){
                     if(item.checked) {
@@ -462,7 +469,7 @@ define(function(require , exports , module){
         },
         // 试读
         inputPreRead:function(){
-            $('.js-file-item').on('keyup',"input[name='preRead']",function(){
+            $('.js-file-item').on('blur',"input[name='preRead']",function(){
                 var preRead = $(this).val();
                 var itemIndex = $(event.target).parents('.doc-li').attr('index');
                 if (itemIndex>-1) {
@@ -475,7 +482,7 @@ define(function(require , exports , module){
                     })
                 }
             })
-            $('.doc-batch-fixed').on('keyup',"input[name='preRead']",function(){
+            $('.doc-batch-fixed').on('blur',"input[name='preRead']",function(){
                 var preRead = $(this).val();
                 uploadObj.uploadFiles.forEach(function(item){
                     if(item.checked) {
