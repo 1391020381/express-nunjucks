e0d71475b1eb02cb5b1183af9b7a5cae047a39e2
zhoujin <171226Q2>
2020-05-28 21:25:42 +0800

    意见反馈bug

--- public/javascripts/src/feedback/init.js
+++ public/javascripts/src/feedback/init.js
@@ -2,4 +2,6 @@ define(function(require , exports , module){
     require('./buyUnlogin')
     require('./index')
     require('./fixedTopBar')
+    require("../common/userMoreMsg")
+    require('../application/suspension')
 });
