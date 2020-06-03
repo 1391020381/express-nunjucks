e5d634262e6b6b33840471d6441dad7df3b74cb5
zhoujin <171226Q2>
2020-06-03 18:26:08 +0800

    修改版本号

---
+++ public/javascripts/.build/upload/init.js
@@ -0,0 +1,4 @@
+define("dist/upload/init", [ "./fixedTopBar", "./upload", "../application/suspension", "../application/method", "../application/checkLogin", "../application/api", "../application/app", "../application/element", "../application/template", "../application/extend", "../common/bilog", "base64", "../cmd-lib/util", "../report/config", "../report/init", "../report/handler", "../report/columns", "../application/helper", "//static3.iask.cn/resource/js/plugins/pc.iask.login.min", "../cmd-lib/toast", "../cmd-lib/upload/Q", "../cmd-lib/upload/Q.Uploader", "./template/list.html", "./template/list_pravite.html" ], function(require, exports, module) {
+    require("./fixedTopBar");
+    require("./upload");
+});
