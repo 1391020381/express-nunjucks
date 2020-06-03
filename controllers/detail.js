75bd6550e0dcf0331d922ca09448b59018c52b71
zhoujin <171226Q2>
2020-06-03 16:48:52 +0800

    放开详情判断文件不存在的逻辑

--- controllers/detail.js
+++ controllers/detail.js
@@ -81,12 +81,12 @@ module.exports = {
                             perMin = fileInfo.permin || '';  // 1:公开、2:私人 3:付费
                             productType = fileInfo.productType
                             uid= fileInfo.uid || ''           // 上传者id
-                            // userID = data.data.uid.slice(0, 10) || ''; //来标注用户的ID，
-                            // if(fileInfo.showflag !=='y'){
-                            //     var results = {showFlag:false}
-                            //     render("detail/index", results, req, res);
-                            //     return
-                            // }
+                            userID = data.data.uid.slice(0, 10) || ''; //来标注用户的ID，
+                            if(fileInfo.showflag !=='y'){
+                                var results = {showFlag:false}
+                                render("detail/index", results, req, res);
+                                return
+                            }
                              if(productType == 6){
                                  if(cuk&&fileInfo.uid == uid){
                                     callback(null, data);
