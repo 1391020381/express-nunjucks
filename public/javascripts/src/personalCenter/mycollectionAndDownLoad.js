939b330b8faa73be4aa7e8a441ccce84e13ee056
zhoujin <171226Q2>
2020-06-17 10:34:02 +0800

    个人中心我的收藏

--- public/javascripts/src/personalCenter/mycollectionAndDownLoad.js
+++ public/javascripts/src/personalCenter/mycollectionAndDownLoad.js
@@ -1,10 +1,11 @@
 define(function(require , exports , module){
    var mycollectionAndDownLoad = require('./template/mycollectionAndDownLoad.html')
-   var _mycollectionAndDownLoadTemplate = template.compile(mycollectionAndDownLoad)({});
    var type = window.pageConfig&&window.pageConfig.page.type
    if(type =='mycollection'){
+      var _mycollectionAndDownLoadTemplate = template.compile(mycollectionAndDownLoad)({type:'mycollection'});
       $(".personal-center-mycollection").html(_mycollectionAndDownLoadTemplate);
    }else{
+      var _mycollectionAndDownLoadTemplate = template.compile(mycollectionAndDownLoad)({type:'mydownloads'});
       $(".personal-center-mydownloads").html(_mycollectionAndDownLoadTemplate);
    }
   
