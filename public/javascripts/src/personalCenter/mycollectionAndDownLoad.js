72616632512bf110de05a989a66eeb430d7a84da
zhoujin <171226Q2>
2020-06-17 10:52:58 +0800

    空数据样式

--- public/javascripts/src/personalCenter/mycollectionAndDownLoad.js
+++ public/javascripts/src/personalCenter/mycollectionAndDownLoad.js
@@ -2,10 +2,10 @@ define(function(require , exports , module){
    var mycollectionAndDownLoad = require('./template/mycollectionAndDownLoad.html')
    var type = window.pageConfig&&window.pageConfig.page.type
    if(type =='mycollection'){
-      var _mycollectionAndDownLoadTemplate = template.compile(mycollectionAndDownLoad)({type:'mycollection'});
+      var _mycollectionAndDownLoadTemplate = template.compile(mycollectionAndDownLoad)({type:'mycollection',list:[]});
       $(".personal-center-mycollection").html(_mycollectionAndDownLoadTemplate);
    }else{
-      var _mycollectionAndDownLoadTemplate = template.compile(mycollectionAndDownLoad)({type:'mydownloads'});
+      var _mycollectionAndDownLoadTemplate = template.compile(mycollectionAndDownLoad)({type:'mydownloads',list:[]});
       $(".personal-center-mydownloads").html(_mycollectionAndDownLoadTemplate);
    }
   
