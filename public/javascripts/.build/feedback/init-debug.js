fe09fbcb97f1dd8277bc0f129de5f9166cfb10b1
zhoujin <171226Q2>
2020-07-03 16:38:15 +0800

    修改版本号

--- public/javascripts/.build/feedback/init-debug.js
+++
@@ -1,7 +0,0 @@
-define("dist/feedback/init-debug", [ "./buyUnlogin-debug", "../cmd-lib/util-debug", "../application/method-debug", "../pay/qr-debug", "../cmd-lib/qr/qrcode.min-debug", "../cmd-lib/qr/jquery.qrcode.min-debug", "../cmd-lib/gioInfo-debug", "../detail/template/buyUnlogin-debug.html", "./index-debug", "../application/suspension-debug", "../application/checkLogin-debug", "../application/api-debug", "../application/app-debug", "../application/element-debug", "../application/template-debug", "../application/extend-debug", "../common/bilog-debug", "base64-debug", "../report/config-debug", "../report/init-debug", "../report/handler-debug", "../report/columns-debug", "../application/helper-debug", "//static3.iask.cn/resource/js/plugins/pc.iask.login.min-debug", "../cmd-lib/toast-debug", "./fixedTopBar-debug", "../common/userMoreMsg-debug" ], function(require, exports, module) {
-    require("./buyUnlogin-debug");
-    require("./index-debug");
-    require("./fixedTopBar-debug");
-    require("../common/userMoreMsg-debug");
-    require("../application/suspension-debug");
-});
--- public/javascripts/.build/special/init.js
+++
@@ -1,7 +0,0 @@
-define("dist/special/init", [ "../cmd-lib/toast", "./bottomBar", "../application/api", "../application/method", "../application/checkLogin", "./content", "../common/userMoreMsg", "../application/app", "../application/element", "../application/template", "../application/extend", "../common/bilog", "base64", "../cmd-lib/util", "../report/config", "../report/init", "../report/handler", "../report/columns", "../application/helper", "//static3.iask.cn/resource/js/plugins/pc.iask.login.min", "../application/suspension" ], function(require, exports, module) {
-    require("../cmd-lib/toast");
-    require("./bottomBar");
-    require("./content");
-    require("../common/userMoreMsg");
-    require("../application/suspension");
-});
