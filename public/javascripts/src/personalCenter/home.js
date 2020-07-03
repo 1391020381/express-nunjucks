988ca0504fb785ce75bdc7e92cd5c4138694ceac
zhoujin <171226Q2>
2020-07-03 10:03:55 +0800

    支付中间页

--- public/javascripts/src/personalCenter/home.js
+++ public/javascripts/src/personalCenter/home.js
@@ -117,7 +117,7 @@ define(function(require , exports , module){
                     // 复用模板,需要转换接口返回的key
                     var data = []
                     if(res.data&&res.data.rows.length){
-                        res.data.rows.forEach(item=>{
+                        res.data.rows.forEach(function(item){
                            data.push({
                             id:1,
                             fileid:item.fid,
--- public/javascripts/src/personalCenter/myorder.js
+++ public/javascripts/src/personalCenter/myorder.js
@@ -51,7 +51,7 @@ define(function(require , exports , module){
                 var formatDate = method.formatDate
                 Date.prototype.format = formatDate
                    if(res.data&&res.data.rows){
-                       res.data.rows.forEach(item=>{
+                       res.data.rows.forEach(function(item){
                           item.payPrice = (item.payPrice/100).toFixed(2)
                           item.orderTime = new Date(item.orderTime).format("yyyy-MM-dd")
                           if(item.refundStatus== 0 ||!item.refundStatus){
