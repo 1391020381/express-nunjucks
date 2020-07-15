28367821fc06a25198683ab2c661db59ab422293
KEY <1065513181@qq.com>
2020-07-15 16:43:15 +0800

    updated

--- public/javascripts/src/index/init.js
+++ public/javascripts/src/index/init.js
@@ -209,6 +209,7 @@ define(function (require,exports,moudle) {
                 $('.userOperateBtn.gocenter').removeClass('hide').siblings('.userOperateBtn').addClass('hide');
                 var expireStr = data.expireTime+'到期'
                 $('.user-state .info-des').text(expireStr);
+                $('.user-state').addClass('vipstate')
                 $('.js-vip-open').hide()
             }else{
                 $('.userOperateBtn.goVip').removeClass('hide').siblings('.userOperateBtn').addClass('hide');
