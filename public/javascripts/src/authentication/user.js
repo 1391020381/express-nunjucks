cd0aaa7ddd8f525542349b65f502cc6f458a29c3
zhoujin <171226Q2>
2020-07-21 18:03:03 +0800

    个人认证修改

--- public/javascripts/src/authentication/user.js
+++ public/javascripts/src/authentication/user.js
@@ -25,7 +25,7 @@ define(function(require , exports , module){
             $('.js-submit').click(function(){
                 // userObj.submitData();
                 if (method.getCookie('cuk')){
-                    orgObj.submitData();
+                    userObj.submitData();
                 }else{
                     isLogin(null,isAutoLogin) 
                 }
