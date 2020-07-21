0cc00595fd10af0b0d291134208f086ed7555712
KEY <1065513181@qq.com>
2020-07-21 21:19:47 +0800

    updated

--- public/javascripts/.build/personalCenter/redirectionURL.js
+++
@@ -1,7 +0,0 @@
-define("dist/personalCenter/redirectionURL", [ "../application/method" ], function(require, exports, module) {
-    var method = require("../application/method");
-    var code = method.getParam("code");
-    var channel = method.getParam("channel");
-    var clientCode = method.getParam("clientCode");
-    window.opener.handleBindThird(code, channel, clientCode);
-});
--- public/javascripts/.build/report/init.js
+++
@@ -1,3 +0,0 @@
-define("dist/report/init", [ "./handler", "../cmd-lib/util", "./columns", "./config", "../application/method" ], function(require, exports, module) {
-    require("./handler");
-});
