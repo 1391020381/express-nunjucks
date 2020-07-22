f6319302ad30d7ea8c6678fb66c9d39db8f3e068
zhoujin <171226Q2>
2020-07-22 16:45:45 +0800

    添加统计代码

--- public/javascripts/src/detail/init.js
+++ public/javascripts/src/detail/init.js
@@ -1,5 +1,4 @@
 define(function (require, exports, module) {
-    require('../common/baidu-statistics.js').initBaiduStatistics('17cdd3f409f282dc0eeb3785fcf78a66')
     require("../cmd-lib/lazyload");
     require("../cmd-lib/myDialog");
     require("../cmd-lib/loading");
@@ -15,5 +14,7 @@ define(function (require, exports, module) {
     require("../common/bilog");
     require('../detail/paradigm4');
     require('../detail/banner.js')
+    require('../common/baidu-statistics.js').initBaiduStatistics('17cdd3f409f282dc0eeb3785fcf78a66')
+    require('../common/baidu-statistics.js').initBaiduStatistics('adb0f091db00ed439bf000f2c5cbaee7')
 }
 );
