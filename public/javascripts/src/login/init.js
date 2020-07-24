a0fd0a361516cbcfd58b438cfe313bcab9eae5c7
zhoujin <171226Q2>
2020-07-24 10:37:23 +0800

    登录dialog基本样式

--- public/javascripts/src/login/init.js
+++ public/javascripts/src/login/init.js
@@ -1,9 +1,9 @@
 define(function(require , exports , module){
     var method = require("../application/method");
     require("../cmd-lib/myDialog");
-    var loginDialog = $('.login-dialog')
+    var loginDialog = $('#login-dialog')
     $("#dialog-box").dialog({
         html: loginDialog.html(),
-        'closeOnClickModal':true
+        'closeOnClickModal':false
     }).open(); 
 });
