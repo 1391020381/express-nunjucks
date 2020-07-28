63a76f6d33af746480d841fc2770f757fcf84d04
zhoujin <171226Q2>
2020-07-28 10:32:58 +0800

    修改详情跳转规则

--- controllers/detail.js
+++ controllers/detail.js
@@ -31,6 +31,39 @@ var requestID_guess = '';  //  个性化数据(猜你喜欢) requestID
 module.exports = {
     render: function (req, res) {
         var _index = {
+             // 查询是否重定向
+             redirectUrl:function(callback) {
+                var opt = {
+                    method: 'POST',
+                    url: appConfig.apiNewBaselPath + Api.file.redirectUrl,
+                    body:JSON.stringify({
+                        sourceLink:req.protocol+'://'+req.hostname+req.url
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                };
+                request(opt,function(err,res1,body){
+                    if(body){
+                        var data = JSON.parse(body);
+                        if (data.code == 0 ){
+                            if (data.data) {
+                                if(data.data.targetLink) {
+                                    var  url =data.data.type ==1? req.protocol+'://'+data.data.targetLink:req.protocol+'://'+req.hostname+'/f/'+data.data.targetLink+'.html';
+                                    res.redirect(url);
+                                    return;
+                                }
+                            }else{
+                                callback(null,null)
+                            }
+                        }else{
+                            callback(null,null)
+                        }
+                    }else{
+                      callback(null,null)
+                    }
+                })
+            },
             list: function (callback) {  // cookies.ui
                 var opt = {
                     method: 'POST',
@@ -390,13 +423,6 @@ module.exports = {
             filePreview: function (callback) {
                 var validateIE9 = ['IE9', 'IE8', 'IE7', 'IE6'].indexOf(util.browserVersion(req.headers['user-agent'])) === -1 ? 0 : 1;
                 server.get(appConfig.apiBasePath + Api.file.preReadPageLimit.replace(/\$fid/, fid).replace(/\$validateIE9/, validateIE9), callback, req, true);
-            },
-            // 查询是否重定向
-            redirectUrl:function(callback) {
-                req.body = {
-                    sourceLink:req.protocol+'://'+req.hostname+req.url
-                }
-                server.post(appConfig.apiNewBaselPath + Api.file.redirectUrl, callback, req, true);
             }
         };
         return async.series(_index, function (err, results) { // async.series 串行无关联
@@ -405,13 +431,7 @@ module.exports = {
                 res.redirect('/node/404.html');
                 return;
             }
-            if (results.redirectUrl && results.redirectUrl.data) {
-                if(results.redirectUrl.data.targetLink) {
-                    var  url =results.redirectUrl.data.type ==1? req.protocol+'://'+results.redirectUrl.data.targetLink:req.protocol+'://'+req.hostname+'/f/'+results.redirectUrl.data.targetLink+'.html';
-                    res.redirect(url);
-                    return;
-                }
-            }
+           
          
          // 转换新对象
              var list = Object.assign({},{data:Object.assign({},results.list.data.fileInfo,results.list.data.transcodeInfo)})
