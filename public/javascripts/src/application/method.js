97ba8701b0a87cca07828a3d9d3bfdea03540f08
zhoujin <171226Q2>
2020-08-13 17:28:12 +0800

    360百度统计初掉document.referrer判断

--- public/javascripts/src/application/method.js
+++ public/javascripts/src/application/method.js
@@ -234,7 +234,7 @@ define(function (require, exports, module) {
             }
         },
         testPhone:function(val){
-            if(/^1(3|4|5|7|8|9)\d{9}$/.test(val)){ 
+            if(/^1(3|4|5|6|7|8|9)\d{9}$/.test(val)){ 
                 return true; 
             }else{
                 return false
