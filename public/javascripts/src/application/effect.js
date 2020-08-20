53622ea3a206a72f9ae49f95b0fe83469bd25dd4
zhoujin <171226Q2>
2020-08-20 14:49:17 +0800

    修改版本号

--- public/javascripts/src/application/effect.js
+++ public/javascripts/src/application/effect.js
@@ -116,7 +116,7 @@ define(function (require, exports, module) {
             $hasLogin.removeClass("user-con-vip");
             // 续费vip
         } else if (data.isVip == 2) {
-            
+            console.log('data.isVip:',data.isVip)
             // $('.vip-title').hide();
         }
 
