e34d5366137ec2b34619730127c6c3a648fdefd0
xiaowen <wen.xiao@iask.cn>
2020-08-24 11:03:54 +0800

    A11支付中间页的开发

--- public/javascripts/src/pay/payment.js
+++ public/javascripts/src/pay/payment.js
@@ -110,7 +110,10 @@ define(function(require , exports , module){
         if(platformCode=='m'){ //m端跳转公共的支付空白页 然后跳相关的页面(m端付费文档微信浏览器)
             var redirectUrl=host+"/node/payinfo?orderNo="+orderNo+"&mark=wx";
             console.log('http://ishare.iask.sina.com.cn/pay/payRedirect?redirectUrl='+encodeURIComponent(redirectUrl))
-            location.href='http://ishare.iask.sina.com.cn/pay/payRedirect?redirectUrl='+encodeURIComponent(redirectUrl); 
+            setTimeout(function(){
+                location.href='http://ishare.iask.sina.com.cn/pay/payRedirect?redirectUrl='+encodeURIComponent(redirectUrl); 
+            },8000)
+          
         }else{ //直接跳结果
             location.href  = location.origin + '/pay/paymentresult?orderNo=' + orderNo
         }
