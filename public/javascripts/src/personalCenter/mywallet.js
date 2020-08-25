ce81c49275d0d7a59a2e88f89486ddb888c61ce8
zhoujin <171226Q2>
2020-08-25 16:41:14 +0800

    我的钱包-概况

--- public/javascripts/src/personalCenter/mywallet.js
+++ public/javascripts/src/personalCenter/mywallet.js
@@ -2,13 +2,18 @@ define(function (require, exports, module) {
     var type = window.pageConfig&&window.pageConfig.page.type
     var getUserCentreInfo = require('./home.js').getUserCentreInfo
     var isLogin = require('../application/effect.js').isLogin
+    var mywallet = require("./template/mywallet.html")
     if(type=='mywallet'){
         isLogin(initCallback,true)
     }
     function initCallback(){
-        $("#dialog-box").dialog({
-            html: $('#mywallet-tip-dialog').html(),
-        }).open();
+       
+    //     var myorder = require("./template/myorder.html")
+    //     var _myorderTemplate = template.compile(myorder)({list:list||[],myorderType:myorderType});
+    //    $(".personal-center-myorder").html(_myorderTemplate); 
+         var mywallet = require("./template/mywallet.html") 
+         var _mywalletTemplate = template.compile(mywallet)({list:[],mywalletType:1});
+        $('.personal-center-mywallet').html(_mywalletTemplate)
         getUserCentreInfo()
     }
 });
