3ade208a179a1e993b9fd81926fcd94e28d97156
zhoujin <171226Q2>
2020-09-25 16:51:43 +0800

    打包資源

--- public/javascripts/.build/404/init.js
+++
@@ -1,5 +0,0 @@
-define("dist/404/init", [ "./index", "../application/suspension", "../application/method", "../application/checkLogin", "../application/api", "../application/urlConfig", "../application/login", "../cmd-lib/jqueryMd5", "../common/bilog", "base64", "../cmd-lib/util", "../report/config", "../cmd-lib/myDialog", "../cmd-lib/toast", "../common/bindphone", "../common/baidu-statistics", "../application/app", "../application/element", "../application/template", "../application/extend", "../application/effect", "../application/helper", "./fixedTopBar" ], function(require, exports, module) {
-    require("./index");
-    require("./fixedTopBar");
-    require("../common/bilog");
-});
