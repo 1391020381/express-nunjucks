87a115026af5c02f2f6477e2276e81c978694bf9
zhoujin <171226Q2>
2020-09-25 17:17:17 +0800

    修改版本號

--- public/javascripts/.build/index/template/saiTemplate-debug.html.js
+++
@@ -1,1 +0,0 @@
-define("dist/index/template/saiTemplate-debug.html", [], "{{each data as item i}}\n<li><a href=\"{{data.type==1?'/f/'+item.id+'.html':data.type==2?item.contentUrl:'/node/s/'+item.id+'.html'}}\" target=\"_blank\">{{data.type==2?item.contentName:item.title}}</a></li>\n{{/each}}  \n");
