d972c985ef932efeb6244961ce31e949e98c09d1
KEY <1065513181@qq.com>
2020-11-19 10:03:19 +0800

    优化上传

--- public/javascripts/src/upload/upload.js
+++ public/javascripts/src/upload/upload.js
@@ -1,3 +1,5 @@
+// const { doUntil } = require('async');
+
 define(function(require , exports , module){
     require('../application/suspension');
     require('./fixedTopBar')
@@ -268,46 +270,42 @@ define(function(require , exports , module){
                 }
             })
         },
+        categorySelect:function(id,id){
+
+        },
         // 分类选择
         categoryOption:function(){
             $('.doc-list').on('click','.js-fenlei',function(e){
                 e.stopPropagation()
                 $(this).siblings('.fenlei').toggle()
-                $(this).siblings('.fenlei').find('.date-con-in').css({width:'140px',overflow: 'hidden scroll'})
+                $(this).parents('.date-con-in').css('width','180px')
+                $('.date-con-second').hide()
+                $('.date-con-third').hide()
             })
-            // $('.doc-list').on('hover','li',function(){
-            //     $(this).addClass('active').siblings('li').removeClass('active');
-            //     // $(this).find('ul li').removeClass('active');
-            //     $(this).parents('.date-con-in').css({overflowY:'auto'})
-               
-            // })
+            $('.doc-list').on('hover','.selectItem li',function(){
+                $(this).addClass('active').siblings('li').removeClass('active');    
+            })
             $('.doc-list').on('hover','.date-con-first>li',function(){
-                $(this).addClass('active').siblings('li').removeClass('active');
-                $('.date-con-sec>li').removeClass('active');
-                $(this).parents('.date-con-in').css({overflowY:'auto'})
-                $(this).parents('.date-con-in').css({width:'282px',overflow: 'hidden scroll'})
-            })
-            $('.doc-list').on('hover','.date-con-first',function(){
-                $(this).parents('.date-con-in').css({width:'282px',overflow: 'hidden scroll'})
-            })
-            $('.doc-list').on('hover','.date-con-sec>li',function(){
-                $(this).addClass('active').siblings('li').removeClass('active');
+                $('.date-con-second>li').removeClass('active');
                 $('.date-con-third>li').removeClass('active');
-                var itemWidth = '282px';
-               if($(this).find('a').attr('last')){
-                    itemWidth = '282px';
-               }else{
-                    itemWidth = '423px';
-                    var height = $('.date-con-sec').height();
-                    $('.date-con-third').height('210px')
-               }
-                $(this).parents('.date-con-in').css({width:itemWidth,overflow: 'hidden scroll'})
+                $('.date-con-second>li').hide()
+                $('.date-con-third').hide()
+                $('.date-con-second').show()
+                var cid  =$(this).find('a').attr('cid')
+                $('.date-con-second').find('.a'+cid).show()
+                $(this).parents('.date-con-in').css('width','370px')
+              
             })
-            $('.doc-list').on('hover','.date-con-third li',function(){
-                $(this).addClass('active').siblings('li').removeClass('active');
-                $(this).parents('.date-con-in').css({width:'423px',overflow: 'hidden scroll'})
+           
+            $('.doc-list').on('hover','.date-con-second>li',function(){
+                $('.date-con-third>li').removeClass('active');
+                $('.date-con-third>li').hide()
+                $('.date-con-third').show()
+                var id2  =$(this).find('a').attr('cid')
+                $('.date-con-third').find('.a'+id2).show()
+                $(this).parents('.date-con-in').css('width','560px')
             })
-            
+          
             $('.doc-list').on('click','.date-con-in li',function(event) {
                 event.stopPropagation()
                 if(!$(this).find('a').attr('last')){
@@ -321,10 +319,10 @@ define(function(require , exports , module){
                     text += $('.date-con-first>li.active a').attr('name');
                     classname = $('.date-con-first>li.active a').attr('name')
                     classid = $('.date-con-first>li.active a').attr('cid')
-                    if ($('.date-con-sec>li.active a').attr('cid')) {
-                        text += '>'+$('.date-con-sec>li.active a').attr('name')
-                        classname = $('.date-con-sec>li.active a').attr('name')
-                        classid = $('.date-con-sec>li.active a').attr('cid')
+                    if ($('.date-con-second>li.active a').attr('cid')) {
+                        text += '>'+$('.date-con-second>li.active a').attr('name')
+                        classname = $('.date-con-second>li.active a').attr('name')
+                        classid = $('.date-con-second>li.active a').attr('cid')
                         if ($('.date-con-third>li.active a').attr('cid')) {
                             text += '>'+$('.date-con-third>li.active a').attr('name')
                             classname = $('.date-con-third>li.active a').attr('name')
