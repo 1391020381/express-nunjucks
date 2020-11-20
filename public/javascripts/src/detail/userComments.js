79e8af73b27cf3b122baa59994d78ed4848e0995
zhoujin <171226Q2>
2020-11-20 10:59:13 +0800

    标签筛选

--- public/javascripts/src/detail/userComments.js
+++ public/javascripts/src/detail/userComments.js
@@ -64,7 +64,7 @@ define(function (require, exports, module){
     $(document).on('click','.doc-main-br .user-comments-container .evaluation-tags',function(){
         var id = $(this).attr('data-id')
         if(id != 'all'){
-            
+            getUserComments(1,id)
         }
     })   
 })
