a31ef9106709de40a00f7c93eaa71fadc7e60262
zhoujin <171226Q2>
2020-11-24 18:42:51 +0800

    首页和个人中心 微认证判断

--- public/javascripts/src/index/init.js
+++ public/javascripts/src/index/init.js
@@ -274,8 +274,8 @@ define(function (require,exports,moudle) {
             var wholeStationVip = data.isMasterVip == 1?'<p class="whole-station-vip"><span class="whole-station-vip-icon"></span><span class="endtime">'+ data.expireTime +'到期</span></p>':''    
             var officeVip = data.isOfficeVip==1?'<p class="office-vip"><span class="office-vip-icon"></span><span class="endtime">'+ data.officeVipExpireTime +'到期</span></p>':''
             var infoDescContent = wholeStationVip + officeVip
-            if(data.isWxAuth){
-                $('.sign-btn').removeClass('.hide')
+            if(!data.isWxAuth){
+                $('.sign-btn').removeClass('hide')
             }
             if(data.isMasterVip == 1 || data.isOfficeVip == 1) {  
                 $('.user-state .vip-icon').addClass('vip-avaliable')
