7f93c5f6b17f9016e08abc5e5b6fdf7cd087f0c8
zhoujin <171226Q2>
2020-11-24 18:46:02 +0800

    打包

---
+++ public/javascripts/.build/pay/init.js
@@ -0,0 +1,6 @@
+define("dist/pay/init", [ "../cmd-lib/tab", "../cmd-lib/toast", "../cmd-lib/myDialog", "./pay", "../common/bilog-module/payVipResult_bilog", "../common/bilog", "base64", "../cmd-lib/util", "../application/method", "../report/config", "../application/urlConfig", "../common/bilog-module/payFileResult_bilog", "../common/bilog-module/payPrivilegeResult_bilog", "swiper", "./qr", "../cmd-lib/qr/qrcode.min", "../cmd-lib/qr/jquery.qrcode.min", "../application/api", "./couponReceive.html", "../application/effect", "../application/checkLogin", "../application/login", "../application/iframe/iframe-messenger", "../application/iframe/messenger", "../common/baidu-statistics", "../common/loginType", "../common/coupon/couponOperate", "../common/coupon/template/options.html", "../common/coupon/couponIssue", "../cmd-lib/loading", "../common/coupon/template/couponCard.html" ], function(require, exports, module) {
+    require("../cmd-lib/tab");
+    require("../cmd-lib/toast");
+    require("../cmd-lib/myDialog");
+    require("./pay");
+});
