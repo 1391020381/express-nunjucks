86c57e5866305f5c6f41e1e4b2e489078380437d
zhoujin <171226Q2>
2020-12-31 13:38:04 +0800

    删除app.js 冗余的 $ajax

--- public/javascripts/src/application/app.js
+++ public/javascripts/src/application/app.js
@@ -115,23 +115,6 @@ function $ajax(url,ajaxMethod,data,async,customHeaders){  //  .done(function(){}
     })
 }
 
-
-function $ajax(url,ajaxMethod,data,async,customHeaders){  //  .done(function(){})  .fail(function(){})
-    return  $.ajax(url, {
-      type: ajaxMethod || "post",
-      data: data?JSON.stringify(data):'',
-      async:(async == undefined||async == '') ?true:false,
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      headers: $.extend({},{
-          'cache-control': 'no-cache',
-          'Pragma': 'no-cache',
-          'Authrization':method.getCookie('cuk')
-      },customHeaders)
-  })
-}
-
-
     $.ajaxSetup({
         headers: {
             'Authrization': method.getCookie('cuk')
