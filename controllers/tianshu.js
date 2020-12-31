ca3fc7bf9583c7a10019761b79273dc35fb86d1e
zhoujin <171226Q2>
2020-12-31 20:31:43 +0800

    修改 tianshuController

--- controllers/tianshu.js
+++ controllers/tianshu.js
@@ -5,19 +5,16 @@ let async = require("async")
 server = require("../models/index")
 
 module.exports = {
-    like: (req, res) => {
-        return async.series({
-            list: function (callback) {
-                req.body = {
-                    "request":{
-                    "userId":req.body.userId,
-                    "requestId":req.body.requestId
-                    }
-                }
-                server.post(`https://tianshu.4paradigm.com/api/v0/recom/recall?sceneID=${req.params.sceneID}`, callback, req);
+    like: async (req, res) => {
+        req.body = {
+            "request":{
+            "userId":req.body.userId,
+            "requestId":req.body.requestId
             }
-        }, function (err, results) {
-            res.send(results.list).end();
-        })
+        }
+        let list = await server.$http(`https://tianshu.4paradigm.com/api/v0/recom/recall?sceneID=${req.params.sceneID}`, 'post', req, res, true)
+       // console.log('list:',JSON.stringify(list))
+        res.send(list).end()
     }
+    
 };
--- routes/tianshu.js
+++ routes/tianshu.js
@@ -3,14 +3,7 @@ var router = express.Router();
 var tianshuController = require('../controllers/tianshu');
 var error = require('../common/error');
 
-router.post('/detail/like/:sceneID', function (req, res, next) {
-    try {
-        console.log('获取猜你喜欢数据=========');
-        tianshuController.like(req, res);
-    } catch (e) {
-        error(req, res, next);
-    }
-});
+router.post('/detail/like/:sceneID', tianshuController.like);
 
 
 module.exports = router;
