7e31f65b5b9ff539a6d454e33359369e79bd6dba
zhoujin <171226Q2>
2021-01-06 17:18:20 +0800

    打包

---
+++ public/javascripts/.build/loginMiddle/index-debug.js
@@ -0,0 +1,48 @@
+define("dist/loginMiddle/index-debug", [ "../application/api-debug", "../application/urlConfig-debug", "../application/method-debug" ], function(require, exports, module) {
+    // 登录 第三方授权回调页面
+    var api = require("../application/api-debug");
+    var method = require("../application/method-debug");
+    var redirectUrl = method.getParam("redirectUrl");
+    var code = method.getParam("code");
+    var channel = method.getParam("channel");
+    // 使用渠道：1:登录；2:绑定
+    var clientCode = method.getParam("clientCode");
+    var jsId = method.getLoginSessionId();
+    thirdLoginRedirect(code, channel, clientCode);
+    function thirdLoginRedirect(code, channel, clientCode) {
+        // 根据授权code 获取 access_token
+        $ajax(api.user.thirdLoginRedirect, "POST", {
+            terminal: "0",
+            thirdType: clientCode,
+            code: code,
+            businessSys: "ishare"
+        }, "", {
+            jsId: jsId
+        }).then(function(res) {
+            if (res.code == "0") {
+                window.loginType = clientCode;
+                // 获取用户信息时埋点需要
+                method.setCookieWithExpPath("cuk", res.data.access_token, res.data.expires_in * 1e3, "/");
+                method.setCookieWithExpPath("loginType", loginType, res.data.expires_in * 1e3, "/");
+                $.ajaxSetup({
+                    headers: {
+                        Authrization: method.getCookie("cuk")
+                    }
+                });
+                $ajax("/node/api/getUserInfo", "GET").then(function(res) {
+                    iask_web.login(res.data.userId);
+                    trackEvent("SE001", "loginResult", "query", {
+                        loginResult: "1",
+                        failMsg: "",
+                        loginType: window.loginType && window.loginType
+                    });
+                    window.open(redirectUrl, "_self");
+                }).fail(function(err) {
+                    window.open(redirectUrl, "_self");
+                });
+            } else {
+                window.open(redirectUrl, "_self");
+            }
+        });
+    }
+});
---
+++ public/javascripts/.build/upload/init-debug.js
@@ -0,0 +1,6 @@
+define("dist/upload/init-debug", [ "./upload-debug", "../application/suspension-debug", "../application/method-debug", "../application/checkLogin-debug", "../application/api-debug", "../application/urlConfig-debug", "../application/login-debug", "../application/loginOperationLogic-debug", "../cmd-lib/jqueryMd5-debug", "../common/bindphone-debug", "../cmd-lib/myDialog-debug", "../cmd-lib/toast-debug", "../application/iframe/iframe-messenger-debug", "../application/iframe/messenger-debug", "../common/baidu-statistics-debug", "../application/app-debug", "../application/element-debug", "../application/template-debug", "../application/extend-debug", "../common/bilogReport-debug", "../application/effect-debug", "../common/loginType-debug", "../application/helper-debug", "../application/single-login-debug", "./fixedTopBar-debug", "../cmd-lib/upload/Q-debug", "../cmd-lib/upload/Q.Uploader-debug", "../cmd-lib/util-debug", "./template/list-debug.html", "./template/list_pravite-debug.html", "./banner-debug", "swiper-debug", "../common/template/swiper_tmp-debug.html", "../common/recommendConfigInfo-debug" ], function(require, exports, module) {
+    // require('./fixedTopBar')
+    require("./upload-debug");
+    require("../application/suspension-debug");
+    require("./banner-debug");
+});
