45b91be553c6e4c3cfd3c1d935c1340ee3d7691c
xiaowen <wen.xiao@iask.cn>
2021-01-14 11:45:10 +0800

    测试详情立即下载埋点统计

--- public/javascripts/src/detail/init.js
+++ public/javascripts/src/detail/init.js
@@ -1,5 +1,5 @@
 define(function (require, exports, module) {
-  
+    require('../common/testBilog');
     require("../cmd-lib/lazyload");
     require("../cmd-lib/myDialog");
     require("../cmd-lib/loading");
