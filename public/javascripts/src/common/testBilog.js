3f93734aea9347a9c6c26c3498d70635d5eaa3cb
xiaowen <wen.xiao@iask.cn>
2021-01-14 21:34:36 +0800

    测试埋点添加

--- public/javascripts/src/common/testBilog.js
+++ public/javascripts/src/common/testBilog.js
@@ -2,21 +2,24 @@
  * 测试立即下载埋点数据直接上报  （以后需删除）
  */
 define(function (require, exports, module) {
-    $(".btn-fix-bottom").on('click',function(){
-        if(window.pageConfig.page.abTest=='a'){
-            trackEvent('SE060', "fileDetailDownClick", 'page', {
-                domID:'btn-fix-bottom-a',
-                domName:'立即下载'
-            });
-        }else{
-            trackEvent('SE060', "fileDetailDownClick", 'page', {
-                domID:'btn-fix-bottom-b',
-                domName:'立即下载'
-            });
+    $(".btn-fix-bottom").on('click',function(e){
+        if(!e.isTrigger){
+            if(window.pageConfig.page.abTest=='a'){
+                trackEvent('SE060', "fileDetailDownClick", 'page', {
+                    domID:'btn-fix-bottom-a',
+                    domName:'立即下载'
+                });
+            }else{
+                trackEvent('SE060', "fileDetailDownClick", 'page', {
+                    domID:'btn-fix-bottom-b',
+                    domName:'立即下载'
+                });
+            }
         }
+
       
     })
-    $(".btn-read-more").on('click',function(){
+    $(".js-read-more").on('click',function(e){
         if(window.pageConfig.page.abTest=='a'){
             if($(this).find(".red-color").text()!=='点击可继续阅读 >'){
                 trackEvent('SE060', "fileDetailDownClick", 'page', {
