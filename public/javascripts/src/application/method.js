df2e9dd11f6b8b796e5fc445995ec1b4bed6e687
zhoujin <171226Q2>
2021-01-20 14:32:02 +0800

    跳转延迟  compatibleIESkip

--- public/javascripts/src/application/method.js
+++ public/javascripts/src/application/method.js
@@ -216,14 +216,16 @@ define(function (require, exports, module) {
          * @param flag 是否新窗口打开
          */
         compatibleIESkip: function (url, flag) {
-            var referLink = document.createElement('a');
-            referLink.href = url;
-            referLink.style.display = 'none';
-            if (flag) {
-                referLink.target = '_blank';
-            }
-            document.body.appendChild(referLink);
-            referLink.click();
+            setTimeout(function(){
+                var referLink = document.createElement('a');
+                referLink.href = url;
+                referLink.style.display = 'none';
+                if (flag) {
+                    referLink.target = '_blank';
+                }
+                document.body.appendChild(referLink);
+                referLink.click();
+            },350)
         },
         testEmail: function (val) {
             var reg = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
