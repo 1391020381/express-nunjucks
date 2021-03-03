2b4b7e56558973e2d24d7b7d18c96540ba6e63df
zhoujin <171226Q2>
2021-03-03 10:38:30 +0800

    app.js

--- app.js
+++ app.js
@@ -1,73 +1,71 @@
 /**
- * @Description: 项目启动文件 
+ * @Description: 项目启动文件
  */
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var nunjucks = require('nunjucks');
-var useragent = require('express-useragent');
-let proxy = require('http-proxy-middleware');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const useragent = require('express-useragent');
+const proxy = require('http-proxy-middleware');
 
-var session = require('express-session');
-//var redisStore = require('connect-redis')(session);
-var log4js = require('./lib/log4js').getLogger('APP');
-var router = require('./routes/index');
-var appConfig = require('./config/app-config');
-var app = express();
+const session = require('express-session');
+// var redisStore = require('connect-redis')(session);
+const log4js = require('./lib/log4js').getLogger('APP');
+const router = require('./routes/index');
+const appConfig = require('./config/app-config');
+const app = express();
 
-//获取useragent
+// 获取useragent
 app.use(useragent.express());
 
-//配置开发环境，端口等
+// 配置开发环境，端口等
 // process.env.ENV = appConfig.env;
 process.env.PORT = appConfig.port;
 
-//视图
+// 视图
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
-//使用 nunjucks templating
-var env = nunjucks.configure(app.get('views'), {
-    autoescape: false,//关闭自动转义 用于渲染富文本
+// 使用 nunjucks templating
+const env = nunjucks.configure(app.get('views'), {
+    autoescape: false, // 关闭自动转义 用于渲染富文本
     noCache: true,
     watch: true,
     express: app
 });
 
 
-
-var helper = require('./helper/helper')(env);
+const helper = require('./helper/helper')(env);
 // set favicon.ico
 app.use(favicon(path.join(__dirname, '/public/images/favicon.ico')));
 
-//本地开发环境反向代理
+// 本地开发环境反向代理
 
-let restream = function (proxyReq, req, res, options) {
+const restream = function (proxyReq, req, res, options) {
     if (req.body) {
-        let bodyData = JSON.stringify(req.body);
+        const bodyData = JSON.stringify(req.body);
         // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
         proxyReq.setHeader('Content-Type', 'application/json');
         proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
         // stream the content
         proxyReq.write(bodyData);
     }
-}
+};
 
 if (appConfig.env == 'local' || appConfig.env == 'debug') {
     app.use('/gateway', proxy({
-        //目标后端服务地址
+        // 目标后端服务地址
         //  target: 'http://ishare.iask.sina.com.cn',
         target: appConfig.newBasePath,
         changeOrigin: true,
         secure: false,
         onProxyReq: restream
-    }))
+    }));
 }
-
 
 
 app.use(logger('dev'));
@@ -96,33 +94,30 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-//中间件，为所有请求记录日志
-app.use(function (req, res, next) {
+// 中间件，为所有请求记录日志
+app.use((req, res, next) => {
     log4js.info(req.url);
     next();
-})
+});
 
 
-//首页
+// 首页
 app.use('/', router);
 
 
-
-
-
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
     if (res.headersSent) {
-        return next(err)
+        return next(err);
     }
     if (appConfig.env === 'local' || appConfig.env === 'dev' || appConfig.env === 'test' || appConfig.env === 'debug') {
-        console.log(err.message)
+        console.log(err.message);
         res.status(err.status || 500);
         res.send({
             status: 0,
             message: err.message,
             error: err,
             statck: err.stack
-        })
+        });
     } else {
         log4js.info({
             status: 0,
@@ -136,8 +131,8 @@ app.use(function (err, req, res, next) {
 });
 
 process.on('uncaughtException', (err) => {
-    console.log('uncaughtException:', err.message, err.stack)
-    process.exit(1)
-})
+    console.log('uncaughtException:', err.message, err.stack);
+    process.exit(1);
+});
 
 module.exports = app;
