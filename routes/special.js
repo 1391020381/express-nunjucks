b9a1d8781a82e4041f332fb32e0db6c1621ac896
zhoujin <171226q2>
2021-04-27 11:48:30 +0800

    专题页修改

--- routes/special.js
+++ routes/special.js
@@ -6,15 +6,21 @@ const specialController = require('../controllers/special');
 // 专题展示页面.
 
 router.get('/node/s/*.html*',function(req,res,next){
+    console.log('专题页/node/s')
     req.mulu = '/node/s'
     next()
 }, specialController.render);
+
 router.get('/t/*.html*',function(req,res,next){
+    console.log('专题页/t')
     req.mulu = '/t'
     next()
 },specialController.render)
+
 router.get('/theme/*.html*',function(req,res,next){
+    console.log('专题页/theme')
     req.mulu = '/theme'
     next()
 },specialController.render)
+
 module.exports = router;
