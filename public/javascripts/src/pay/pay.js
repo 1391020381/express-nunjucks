68ab5199f3c2487064bc1434ad99b6ce4e9a0fd0
zhoujin <171226q2>
2021-04-28 17:39:58 +0800

    suspension-xxxxxxxxx

--- public/javascripts/src/pay/pay.js
+++ public/javascripts/src/pay/pay.js
@@ -442,8 +442,8 @@ define(function (require, exports, moudle) {
             s.parentNode.insertBefore(j, s);
         })(window, document, 'script', '_MEIQIA');
         _MEIQIA('entId', 'da3025cba774985d7ac6fa734b92e729');
+       _MEIQIA('manualInit');
         _MEIQIA('subSource', 'sub_source_10');
-       // _MEIQIA('manualInit');
       _MEIQIA('init');
     } catch (e) {
         console.log('美洽加载错误', JSON.stringify(e));
