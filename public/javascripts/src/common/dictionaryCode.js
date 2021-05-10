1dd0d3516ea80977f050fae9adf7071e8b690644
zhoujin <171226q2>
2021-05-10 10:57:48 +0800

    个人认证和机构认证 用户协议

---
+++ public/javascripts/src/common/dictionaryCode.js
@@ -0,0 +1,9 @@
+// 字典接口code
+
+define(function (require, exports, module) {
+    return {
+        details:{
+            paidTest:'PaidTest'   // 详情vip专题文档 通过字典配置相关信息,以付费文档的形式购买
+        }
+    };
+});
