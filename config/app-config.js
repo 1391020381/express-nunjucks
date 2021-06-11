7393072b5be6550cc7dc6ba864cf563b4c88ca74
wangceyu <ceyu.wang@iask.cn>
2021-06-11 09:57:53 +0800

    A26:更新埋点sdk--By 王策宇

--- config/app-config.js
+++ config/app-config.js
@@ -23,14 +23,10 @@ const config = {
     },
     local: {
         env: env,
-        // apiBasePath: 'http://dev-gateway.pc.ishare.iasktest.com/gateway/pc',
-        // apiNewBaselPath: 'http://dev-gateway.pc.ishare.iasktest.com/gateway',
-        // newBasePath: 'http://dev-gateway.pc.ishare.iasktest.com',
-        // loginUrl: 'http://dev-login-ishare.iask.com.cn/gateway/cas/login/jump?redirectUrl=',
-        apiBasePath: 'http://test-gateway.pc.ishare.iasktest.com/gateway/pc',
-        apiNewBaselPath: 'http://test-gateway.pc.ishare.iasktest.com/gateway',
-        newBasePath: 'http://test-gateway.pc.ishare.iasktest.com',
-        loginUrl: 'http://test-login-ishare.iask.com.cn/gateway/cas/login/jump?redirectUrl=',
+        apiBasePath: 'http://dev-gateway.pc.ishare.iasktest.com/gateway/pc',
+        apiNewBaselPath: 'http://dev-gateway.pc.ishare.iasktest.com/gateway',
+        newBasePath: 'http://dev-gateway.pc.ishare.iasktest.com',
+        loginUrl: 'http://dev-login-ishare.iask.com.cn/gateway/cas/login/jump?redirectUrl=',
         // 日志输入路径
         logPath: '/data/logs/node-pc/',
         // 服务端口-自定义
